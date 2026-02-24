64c19cd90908a3ad38356be1423ee99b8dd02a96
Ali Khalaji <khalajicompany@gmail.com>
2026-02-24 14:33:52 +0330

    Change api

--- composables/product.ts
+++ composables/product.ts
@@ -263,9 +263,8 @@ export class Product {
   }) {
     try {
       const res = await this.fetchWithAuth<IApiResponse<IProductFull>>(
-        `${this.config.public.baseUrl}/get-products-full`,
-        { method: 'POST', body: { ...data } },
-        true
+        `${this.config.public.baseUrl}/api/get-products-full`,
+        { method: 'POST', body: { ...data } }
       );
 
       console.log(res);
@@ -406,7 +405,7 @@ export class Product {
   async apiGetFolderProduct(data?: any) {
     try {
       const res = await this.fetchWithAuth<IApiResponse<TSearchNewProduct>>(
-        `${this.config.public.baseUrl}/get_products_by_brand_and_tire`,
+        `${this.config.public.baseUrl}/api/get-products-by-brand-and-tire`,
         { method: 'POST', body: { ...data } },
         true
       );
